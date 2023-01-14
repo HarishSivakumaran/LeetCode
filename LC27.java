8ee6fdfd91e7780ca41d1ddd6b52d6b2f4215936
Sivakumaran <harish.sivakumaran@sap.com>
2023-01-14 22:31:08 +0530

    27. Remove Element

---
+++ LC27.java
@@ -0,0 +1,17 @@
+// 27. Remove Element
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+       
+        int valIndex = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                nums[valIndex] = nums[i];
+                valIndex++;
+            }
+        }
+        return valIndex;
+        
+    }
+}
