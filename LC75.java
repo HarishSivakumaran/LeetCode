7f288af41c3c25ac07b2f22c095b8b71afd8cc30
Harish Sivakumaran <harish.sivakumaran@sap.com>
2023-01-24 08:11:37 +0530

    75. Sort Colors

---
+++ LC75.java
@@ -0,0 +1,22 @@
+// 75. Sort Colors
+
+class Solution {
+    public void sortColors(int[] nums) {        
+        int l = 0, i = 0, r = nums.length-1, temp = 0;
+
+        while(i <= r){
+            if(nums[i] == 0){
+                temp = nums[i];
+                nums[i] = nums[l];
+                nums[l] = temp;
+                l++;
+            }else if(nums[i] == 2){
+                temp = nums[i];
+                nums[i] = nums[r];
+                nums[r] = temp;
+                r--;
+            }
+            i++;
+        }        
+    }
+}
