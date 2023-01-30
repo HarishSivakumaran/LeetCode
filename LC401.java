669694dbde7252ead02025f0d212d28b60cd4379
Harish Sivakumaran <harish.sivakumaran@sap.com>
2023-01-30 09:56:27 +0530

    401. Binary Watch

---
+++ LC401.java
@@ -0,0 +1,16 @@
+// 401. Binary Watch
+
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> out = new ArrayList();
+        for(int h = 0; h < 12; h++){
+            for(int m = 0; m < 60; m++){
+                if(Integer.bitCount(h) + Integer.bitCount(m) == turnedOn){
+                    out.add(String.format("%d:%02d", h, m));
+                }
+            }
+        }
+        return out;
+        
+    }
+}
