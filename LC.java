5d3907326f5363bbddd3ce62d906c42d001ea1d0
Harish Sivakumaran <harish.sivakumaran@sap.com>
2023-02-04 18:32:32 +0530

    521. Longest Uncommon Subsequence I

---
+++ LC.java
@@ -0,0 +1,9 @@
+// 521. Longest Uncommon Subsequence I
+
+class Solution {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b))
+            return -1;
+        return Math.max(a.length(), b.length());
+    }
+}
