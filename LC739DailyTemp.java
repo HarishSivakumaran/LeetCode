7a7fb9b1038b415a791c7ff862c835d2b3b45847
Harish Sivakumaran <harish.sivakumaran@sap.com>
2023-02-12 12:48:47 +0530

    739. Daily Temperatures

---
+++ LC739DailyTemp.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] dailyTemperatures(int[] t) {
+        Stack<Integer> stack = new Stack();
+        for (int i = 0; i < t.length; i++) {
+            while (stack.size() > 0 && t[stack.peek()] < t[i]) {
+                t[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+        while (stack.size() > 0) {
+            t[stack.pop()] = 0;
+        }
+
+        return t;
+
+    }
+}
