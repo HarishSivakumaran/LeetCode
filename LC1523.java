876eef079bf618b0715e68b9b1d962acf0c16ba9
Harish Sivakumaran <harish.sivakumaran@sap.com>
2023-02-13 12:29:18 +0530

    1523. Count Odd Numbers in an Interval Range

---
+++ LC1523.java
@@ -0,0 +1,12 @@
+// 1523. Count Odd Numbers in an Interval Range
+
+class Solution {
+    public int countOdds(int low, int high) {
+        int count = (high - low) / 2;
+        if (low % 2 != 0 || high % 2 != 0)
+            count++;
+
+        return count;
+
+    }
+}
