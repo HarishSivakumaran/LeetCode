e29c10d7d3c8def363605a6197b10df17fabc74f
Harish Sivakumaran <harish.sivakumaran.7@gmail.com>
2023-09-28 11:40:50 +0530

    905. Sort Array By Parity

---
+++ 905. Sort Array By Parity.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int insPtr = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if((nums[i] & 1) == 0) {
+                //even
+                int temp = nums[i];
+                nums[i] = nums[insPtr];
+                nums[insPtr++] = temp;
+            }
+        }
+
+        return nums;
+    }
+}
